2cdd8edf25ae8dce5c30d7b130dc531e93333a40
Pablo Santini <pjsantini@gmail.com>
2015-08-12 23:59:40 -0430

    Quiz con Creditos

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,15 @@ var quiz_controller = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+    res.render('index', { title: 'Quiz' });
 });
 
 router.get('/quizes/question', quiz_controller.question);
 router.get('/quizes/answer', quiz_controller.answer);
+
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+    res.render('author');
+});
 
 module.exports = router;
