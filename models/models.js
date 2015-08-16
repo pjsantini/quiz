7cd1dfbf21993df0ba0d9758de1576306123c16b
Pablo Santini <pjsantini@gmail.com>
2015-08-16 16:50:27 -0430

    Quizes con Busqueda

--- models/models.js
+++ models/models.js
@@ -4,12 +4,31 @@
 
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var passwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-//Usar BD SQLite
-var sequelize = new Sequelize(null, null, null,
-                        {dialect: "sqlite", storage: "quiz.sqlite"}
+//Usar BD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, passwd,
+                        {
+                            dialect: dialect,
+                            protocol : protocol,
+                            port: port,
+                            host: host,
+                            storage: storage,
+                            omitNull: true
+                        }
                     );
 //Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
@@ -23,8 +42,11 @@ sequelize.sync().success(function() {
         if (count === 0) {
             Quiz.create({   pregunta: 'Capital de Italia',
                             respuesta: 'Roma'
+            });
+            Quiz.create({   pregunta: 'Capital de Portugal',
+                            respuesta: 'Lisboa'
             })
-            .success(function() {
+            .then(function() {
                 console.log('Base de Datos Inicializada')
             });
         };
