cee8d1784460d4660e63a8f58cc9d96241cb9f8d
Pablo Santini <pjsantini@gmail.com>
2015-08-18 22:28:33 -0430

    Autenticación y sesión en Quizes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
 var routes = require('./routes/index');
+var session = require('express-session');
 
 var app = express();
 
@@ -17,14 +18,26 @@ app.set('view engine', 'ejs');
 app.use(partials());
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Helpers dinamicos:
+app.use(function(req, res, next) {
+
+    // guardar path en session.redir para despues de login
+    if (!req.path.match(/\/login|\/logout|\/user/)) {
+        req.session.redir = req.path;
+    }
+    res.locals.session = req.session;
+    next();
+});
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quiz_controller = require('../controllers/quiz_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,12 @@ router.get('/', function(req, res, next) {
 //Autoload de comandos de :quizId
 router.param('quizId', quiz_controller.load);
 
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
+
+// Definición de rutas de /quizes
 router.get('/quizes', quiz_controller.index);
 router.get('/quizes/:quizId(\\d+)', quiz_controller.show);
 router.get('/quizes/:quizId(\\d+)/answer', quiz_controller.answer);
